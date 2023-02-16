2ea36132681ecc7174597e7f898ed431bbc5286b
Sarah Whalley <s.whalley1130@gmail.com>
2023-02-16 10:27:31 -0700

    Creates function to generate object given array, and given multiple array

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+let exampleArray=['Sarah',"Whalley","Student",20]
+let exampleArray2=["John","Smith","Dev", 40]
+let exampleArray3=[['Sarah',"Whalley","Student",20],["John","Smith","Dev", 40]]
+
+
+function createEmployeeRecord(array)
+{
+    let record=
+    {
+        firstName:array[0],
+        familyName:array[1],
+        title:array[2],
+        payPerHour:array[3],
+        timeInEvents:[],
+        timeOutEvents:[],
+    }
+    return record;
+}
+
+function createEmployeeRecords(arrayOfArrays)
+{
+    let employRecords=[];
+    arrayOfArrays.forEach(array=>
+        {
+            let record=createEmployeeRecord(array);
+            employRecords.push(record);
+        })
+    return employRecords;
+}
+
+
+
+
+const newRecords=createEmployeeRecords(exampleArray3)
+console.log(newRecords)
