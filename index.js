a2d3809f384d0238add1d1dec685ea8d3e41fac5
Sarah Whalley <s.whalley1130@gmail.com>
2023-02-16 11:21:42 -0700

    Creates functions to calculate hours worked on given day, and pay for given day

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Your code here
 
+const { copySync } = require("file-system");
+
 let exampleArray=['Sarah',"Whalley","Student",20]
 let exampleArray2=["John","Smith","Dev", 40]
 let exampleArray3=[['Sarah',"Whalley","Student",20],["John","Smith","Dev", 40]]
@@ -24,14 +26,65 @@ function createEmployeeRecords(arrayOfArrays)
     let employRecords=[];
     arrayOfArrays.forEach(array=>
         {
-            let record=createEmployeeRecord(array);
-            employRecords.push(record);
+            employRecords.push(createEmployeeRecord(array));
         })
     return employRecords;
+}
+
+function createTimeInEvent(empRecord,dateStamp)
+{
+    let newTimeIn=
+    {
+        type: 'TimeIn',
+        hour: parseInt(dateStamp.split(' ')[1]),
+        date: dateStamp.split(' ')[0]
+    }
+    empRecord.timeInEvents.push(newTimeIn);
+    return empRecord;
+}
+
+function createTimeOutEvent(empRecord,dateStamp)
+{
+    let newTimeOut=
+    {
+        type: 'TimeOut',
+        hour: parseInt(dateStamp.split(' ')[1]),
+        date: dateStamp.split(' ')[0]
+    }
+    empRecord.timeOutEvents.push(newTimeOut);
+    return empRecord;
+}
+
+function hoursWorkedOnDate(empRecord,date)
+{
+    let index=0;
+    let hoursWorked=0;
+    empRecord.timeInEvents.forEach(record=>
+        {
+            for (const value in record)
+            {
+            if(record[value]===date)
+            {
+               hoursWorked=empRecord.timeOutEvents[index].hour-empRecord.timeInEvents[index].hour;
+            }
+            }
+            index++;
+        })
+        return hoursWorked/100;
+}
+
+function wagesEarnedOnDate(empRecord,date)
+{
+ hoursWorkedOnDate(empRecord,date)*empRecord.payPerHour;
 }
 
 
 
 
-const newRecords=createEmployeeRecords(exampleArray3)
-console.log(newRecords)
+let newRecord=createEmployeeRecord(exampleArray)
+createTimeInEvent(newRecord,"2023-02-15 0800")
+createTimeOutEvent(newRecord,"2023-02-15 1600")
+createTimeInEvent(newRecord,"2023-02-16 0700")
+createTimeOutEvent(newRecord,"2023-02-16 1200")
+let hours=hoursWorkedOnDate(newRecord,"2023-02-15")
+console.log(hours)
